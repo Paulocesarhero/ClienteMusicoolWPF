d70cc192e7342937c3f4970c723461a096b96b69
Paulo <paulocesarhero@gmail.com>
2023-05-12 08:55:42 -0600

    Implementacion de autentificacion basica

--- musicoolClientWPF/model/api/UsuarioServices.cs
+++ musicoolClientWPF/model/api/UsuarioServices.cs
@@ -13,12 +13,14 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace musicoolClientWPF.model.api
 {
     internal class UsuarioServices
     {
         private string _UrlBase { get; set; }
+        private BasicAuth _BasicAuth { get; set; } = new BasicAuth();
 
         public UsuarioServices()
         {
@@ -32,6 +34,9 @@ namespace musicoolClientWPF.model.api
                 string json = r.ReadToEnd();
                 dynamic data = JObject.Parse(json);
                 _UrlBase = data.configApi.urlBase;
+                _BasicAuth.Cliente = data.configApi.cliente;
+                _BasicAuth.Contra = data.configApi.contra;
+
             }
         }
         public async Task<RespuestaUsuario> getTokenAcceso(Usuario usuario)
@@ -45,8 +50,13 @@ namespace musicoolClientWPF.model.api
                 new KeyValuePair<string, string>("username", usuario.username),
                 new KeyValuePair<string, string>("password", usuario.password)
             };
+            string authInfo = "${_BasicAuth.Cliente}:{_BasicAuth.Contra}";
+            authInfo = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(authInfo));
+            AuthenticationHeaderValue authHeader = new AuthenticationHeaderValue("Basic", authInfo);
+            httpClient.DefaultRequestHeaders.Authorization = authHeader;
             var request = new HttpRequestMessage(HttpMethod.Post, urlBase);
             request.Content = new FormUrlEncodedContent(keyValues);
+            
             try
             {
                 var response = await httpClient.SendAsync(request);
---
+++ musicoolClientWPF/model/objetos/BasicAuth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicoolClientWPF.model.objetos
+{
+    internal class BasicAuth
+    {
+        public string Cliente { get; set; }
+
+        public string Contra { get; set; }
+    }
+}
