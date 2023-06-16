85fa287118b93f3e6019f64e07b26b6ba4e19fbe
Paulo <paulocesarhero@gmail.com>
2023-06-15 23:26:22 -0600

    Ultima modificacion para que los foros se vean bien

--- musicoolClientWPF/model/objetos/CancionRespuesta.cs
+++ musicoolClientWPF/model/objetos/CancionRespuesta.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace musicoolClientWPF.model.objetos
 {
     public class CancionRespuesta
     {
+        [JsonProperty("id")]
         public string id { get; set; }
+
+        [JsonProperty("nombre")]
         public string nombre { get; set; }
+
+        [JsonProperty("artista")]
         public string artista { get; set; }
+
+        [JsonProperty("fechaD")]
         public string fechaDePublicacion { get; set; }
     }
 }
